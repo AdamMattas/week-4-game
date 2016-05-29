08f2fdd496b9157c8ad40742150e68caded1bde1
adam fader <adam fader>
2016-05-28 21:01:21 -0400

    added comments to make code easier to follow

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,40 +10,29 @@ var vader = {
 	name: "Darth Vader",
   health: 145,
 	attack: 8,
-	counter: 20,
-	fight: false,
-	alive: true,
-	enemy: true
+	counter: 20
 };
 var skywalker = {
   name: "Luke Skywalker",
   health: 130,
 	attack: 9,
-	counter: 5,
-	fight: false,
-	alive: true,
-	enemy: true
+	counter: 5
 };
 var yoda = {
   name: "Master Yoda",
   health: 160,
 	attack: 5,
-	counter: 25,
-	fight: false,
-	alive: true,
-	enemy: true
+	counter: 25
 };
 var obi = {
   name: "Obi Wan Kenobi",
   health: 120,
 	attack: 8,
-	counter: 15,
-	fight: false,
-	alive: true,
-	enemy: true
+	counter: 15
 };
 
 $(document).ready(function(){
+	//Character stats displayed on card
 	$('#attack-sky').html(skywalker.attack);
 	$('#counter-sky').html(skywalker.counter);
 	$('#attack-obi').html(obi.attack);
@@ -53,19 +42,18 @@ $(document).ready(function(){
 	$('#attack-vader').html(vader.attack);
 	$('#counter-vader').html(vader.counter);
 
-	//Select the character to fight as for entire game & move to arena
+	//Select the character to fight as for entire game, set computer variable and & move to arena
 	//Move enemies to the enemy section of the screen
+	//Movements done by adding and removing styles
 	$(document).on('click', '.character', function(){
       player = $(this).data('character');
       if(player == 'vader'){
       	player = vader;
-      	player.enemy = false;
       	$('#vader').removeClass('character').addClass('friend');
       	$('#vader-left').removeClass('hide');
       	$('#skywalker, #yoda, #obi').removeClass('character').addClass('enemy');
       }else if(player == "skywalker"){
       	player = skywalker;
-      	player.enemy = false;
       	$('#skywalker').removeClass('character').addClass('friend');
       	$('#skywalker-left').removeClass('hide');
       	$('#vader, #yoda, #obi').removeClass('character').addClass('enemy');
@@ -82,14 +70,18 @@ $(document).ready(function(){
       	$('#obi-left').removeClass('hide');
       	$('#skywalker, #vader, #yoda').removeClass('character').addClass('enemy');
       }
+      //Hide start instruction
       $('#select-left, #select-right').addClass('hide');
+      //Add player name and health bar
       $('#name-left').html(player.name);
       progressBarLeft(player.health);
       $('#saber-on').get(0).play();
+      //Show enemy start instruction
       $('#select-middle').removeClass('hide');
   });
 
-	//On particular enemy click, set as in fight and move to arena
+	//On particular enemy click, set computer variable and set as in fight and move to arena
+	//Movements done by adding and removing styles
 	$(document).on('click', '.enemy', function(){
 		computer = $(this).data("character");
 		if(computer == 'vader'){
@@ -113,24 +105,26 @@ $(document).ready(function(){
       	$('#obi').removeClass('enemy').addClass('enemy-fight');
       	$('#obi-right').removeClass('hide');
       }
+      //Hide victory & defeat messages & show attack button
       $('#select-middle').addClass('hide');
       $('#victory-message').addClass('hide');
 			$('#victory-name').addClass('hide');
 			$('#defeat-message').addClass('hide');
 			$('#defeat-name').addClass('hide');
-      $('#name-right').html(computer.name);
       $('#attack').removeClass('hide');
+      //Transparent panel to block or allow enemy click
       $('#side').removeClass('hide');
+      //Add player name and health bar
+      $('#name-right').html(computer.name);
       progressBarRight(computer.health);
       $('#saber-on').get(0).play();
 	});
 
-	//If game has not been won, the player will pick a new enemy to fight
-
 	//On attack button click, the player will attack the enemy 1 time and deal damage
-	//Player's power increases by 6 on each attack
+	//Player's power increases by base number on each attack
 		$(document).on('click', '#attack', function(){
 			if(player.health > 0 && computer.health > 0){
+				//Subtract computer health from player attack
 				computer.health -= player.attack;
 				if(player.name == 'Luke Skywalker'){
 					player.attack+=9;
@@ -151,37 +145,40 @@ $(document).ready(function(){
 					player.health -= computer.counter;
 				}else if(computer.health <= 0){
 					wins++;
+					//Hides attack button between rounds
 					$('#attack').addClass('hide');
+					//Transparent panel to block or allow enemy click
 					$('#side').addClass('hide');
-					//Check if game has been won
+					//Check if all enemies have been defeated
+					//If game is over, display message and hide attack button
 					if(wins == 3){
 						$('#victory-message').removeClass('hide');
 						$('#victory-name').removeClass('hide');
 						$('#victory-name').html('all of your enemies!');
 						$('#attack').addClass('hide');
 					}else if (computer.name == 'Darth Vader'){
-						//If enemy has been defeated remove from defender area
+						//If enemy has been defeated remove from defender area and display message
 						$('#vader').removeClass('enemy-fight').addClass('defeated');
 						$('#vader-right').addClass('hide');
 						$('#victory-message').removeClass('hide');
 						$('#victory-name').removeClass('hide');
 						$('#victory-name').html(computer.name + '!');
 					}else if (computer.name == 'Luke Skywalker'){
-						//If enemy has been defeated remove from defender area
+						//If enemy has been defeated remove from defender area and display message
 						$('#skywalker').removeClass('enemy-fight').addClass('defeated');
 						$('#skywalker-right').addClass('hide');
 						$('#victory-message').removeClass('hide');
 						$('#victory-name').removeClass('hide');
 						$('#victory-name').html(computer.name + '!');
 					}else if (computer.name == 'Master Yoda'){
-						//If enemy has been defeated remove from defender area
+						//If enemy has been defeated remove from defender area and display message
 						$('#yoda').removeClass('enemy-fight').addClass('defeated');
 						$('#yoda-right').addClass('hide');
 						$('#victory-message').removeClass('hide');
 						$('#victory-name').removeClass('hide');
 						$('#victory-name').html(computer.name + '!');
 					}else{
-						//If enemy has been defeated remove from defender area
+						//If enemy has been defeated remove from defender area and display message
 						$('#obi').removeClass('enemy-fight').addClass('defeated');
 						$('#obi-right').addClass('hide');
 						$('#victory-message').removeClass('hide');
@@ -190,14 +187,17 @@ $(document).ready(function(){
 					}
 				}
 			}
+			//Update player and computer health bar
 			//Check if player's health is at or below zero
 			progressBarRight(computer.health);
 			progressBarLeft(player.health);
+			//Play lightsaber attack sound
 			$('#fight-on').get(0).play();
 			defeatCheck();
 		});
 
 		//Check if player's health is at or below zero
+		//If player has lost, the defeat message shows and the attack button is hidden
 		function defeatCheck(){
 			if(player.health <= 0){
 				$('#defeat-message').removeClass('hide');
@@ -207,6 +207,7 @@ $(document).ready(function(){
 			}
 		};
 
+			//Changes width of health bar based on individual character health percent
 		 function progressBarLeft(percent) {
 		 	if(player.name == 'Luke Skywalker'){
 		 		$('#progress-left').animate({
@@ -227,6 +228,7 @@ $(document).ready(function(){
 		 	}
      };
 
+     //Changes width of health bar based on individual character health percent
      function progressBarRight(percent) {
      	if(computer.name == 'Luke Skywalker'){
 		 		$('#progress-right').animate({
